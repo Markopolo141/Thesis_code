0140152a1416f2947ff55450e1a276a1f90e1b59
markopolo141 <markburgess1989@hotmail.com>
2020-04-10 18:03:04 +1000

    update

---
+++ GNK_experiments/experiment6/compile_output1.sh
@@ -0,0 +1,4 @@
+python format_output.py lmp_out.json lmp_out_formatted.txt
+python format_output.py vcg_out.json vcg_out_formatted.txt
+python format_output.py gnk_out.json gnk_out_formatted.txt
+dot2tex -ftikz 1.dot -o 1.tex
--- GNK_experiments/experiment6/format_output.py
+++ GNK_experiments/experiment6/format_output.py
@@ -1,11 +1,20 @@
 import json
+import click
 
-with open("g.json","r") as f:
-	data = json.load(f)
-	for i in range(len(data[0])):
-		print "({},{})".format(i,data[0][i]),
-	print ""
-	print ""
-	for i in range(len(data[1])):
-		print "({},{})".format(i,data[1][i]),
+@click.command()
+@click.argument('input_file', type=click.File('r'))
+@click.argument('output_file', type=click.File('w'))
+def run(input_file, output_file):
+	#data = json.load(f)
+	lines = input_file.readlines()
+	for l in lines:
+		l = "[{}".format(l.split("[")[1])
+		data = json.loads(l)
+		for i in range(len(data)):
+			output_file.write("({},{})".format(i,data[i]))
+		output_file.write("\n")
+	print "written output"
 	
+
+if __name__ == '__main__':
+    run()
