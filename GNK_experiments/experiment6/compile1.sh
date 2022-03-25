f858b8d73b7b9fc76c9998637b9a01305dc3bb38
Mark <markburgess1989@gmail.com>
2022-03-25 16:04:06 +1100

    update thesis

--- GNK_experiments/experiment6/compile1.sh
+++ GNK_experiments/experiment6/compile1.sh
@@ -6,3 +6,4 @@ python network_generator.py 90 115 1
 python lmp.py 1.json lmp_out.json
 python vcg.py 1.json vcg_out.json
 python bounds_solver.py 1.json gnk_out.json 8 0.02 0.2 10 new
+python bounds_solver.py 1.json shap_out.json 8 0.02 0.2 10 newnew
